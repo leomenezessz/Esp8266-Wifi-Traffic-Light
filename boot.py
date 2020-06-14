d72d6336dd655097dc01ac6fcfd85704388a7902
Leonardo Menezes <leonardosmenezes.ssz@gmail.com>
2020-06-14 15:15:16 -0300

    Remove ssid credentials

--- boot.py
+++ boot.py
@@ -5,10 +5,10 @@ logger = Logger(__name__)
 
 
 def main():
-    wifi = WifiManager('Americana 2.4G', 'lully360')
+    wifi = WifiManager('your-ssid', 'your-network-password')
 
     if wifi.is_connected():
-        logger.info("Connected to the network : " + wifi.network_config())
+        logger.info("Connected to the network : " + wifi.show_networks())
 
 
 if __name__ == '__main__':
